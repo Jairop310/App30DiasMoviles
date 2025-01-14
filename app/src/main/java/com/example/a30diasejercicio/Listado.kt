495011d3908d43e23bad8efe73eac8a5246c4a5b
Luis Anselmo <luissiiss23@gmail.com>
2025-01-14 10:22:25 +0100

    liastado e imagenes

--- app/src/main/java/com/example/a30diasejercicio/Listado.kt
+++ app/src/main/java/com/example/a30diasejercicio/Listado.kt
@@ -3,7 +3,35 @@ package com.example.a30diasejercicio
 class Listado {
     fun loadEquipos(): List<Tarjeta> {
         return listOf<Tarjeta>(
-            Tarjeta("Paco",2024,"2 Anillos, 3 Campeonatos" , R.drawable.paco),
-            Tarjeta("Luis",2024,"2 Anillos, 3 Campeonatos" , R.drawable.luis))
+            Tarjeta(R.string.atlanta_hawks, 2024, 1, R.drawable.atlanta_hawks),
+            Tarjeta(R.string.boston_celtics, 2024, 17, R.drawable.celtics),
+            Tarjeta(R.string.brooklyn_nets,2024, 2, R.drawable.brooklyn),
+            Tarjeta(R.string.charlotte_hornets, 2024, 0, R.drawable.hornets),
+            Tarjeta(R.string.chicago_bulls, 2024, 6, R.drawable.bulls),
+            Tarjeta(R.string.cleveland_cavaliers, 2024, 1, R.drawable.cavaliers),
+            Tarjeta(R.string.dallas_mavericks, 2024, 1, R.drawable.maverivks),
+            Tarjeta(R.string.denver_nuggets, 2024, 1, R.drawable.nuggets),
+            Tarjeta(R.string.detroit_pistons, 2024, 3, R.drawable.detroit),
+            Tarjeta(R.string.golden_state_warriors, 2024, 7, R.drawable.warriors),
+            Tarjeta(R.string.houston_rockets, 2024, 2, R.drawable.rockets),
+            Tarjeta(R.string.indiana_pacers, 2024, 0, R.drawable.pacers),
+            Tarjeta(R.string.los_angeles_clippers, 2024, 0, R.drawable.clippers),
+            Tarjeta(R.string.los_angeles_lakers, 2024, 17, R.drawable.lakers),
+            Tarjeta(R.string.memphis_grizzlies, 2024, 0, R.drawable.grizzlies),
+            Tarjeta(R.string.miami_heat, 2024, 3, R.drawable.miami),
+            Tarjeta(R.string.milwaukee_bucks, 2024, 2, R.drawable.bucks),
+            Tarjeta(R.string.minnesota_timberwolves, 2024, 0, R.drawable.timberwolves),
+            Tarjeta(R.string.new_orleans_pelicans, 2024, 0, R.drawable.new_orleans),
+            Tarjeta(R.string.new_york_knicks, 2024, 2, R.drawable.knicks),
+            Tarjeta(R.string.oklahoma_city_thunder, 2024, 1, R.drawable.thunder),
+            Tarjeta(R.string.orlando_magic, 2024, 0, R.drawable.orlando),
+            Tarjeta(R.string.philadelphia_76ers, 2024, 3, R.drawable.philadelphia),
+            Tarjeta(R.string.phoenix_suns, 2024, 0, R.drawable.suns),
+            Tarjeta(R.string.portland_trail_blazers, 2024, 1, R.drawable.portland),
+            Tarjeta(R.string.sacramento_kings, 2024, 1, R.drawable.kings),
+            Tarjeta(R.string.san_antonio_spurs, 2024, 5, R.drawable.spurs),
+            Tarjeta(R.string.toronto_raptors, 2024, 1, R.drawable.toronto),
+            Tarjeta(R.string.utah_jazz, 2024, 0, R.drawable.jazz__1_),
+            Tarjeta(R.string.washington_wizards, 2024, 1, R.drawable.wizzards))
     }
 }
--- app/src/main/java/com/example/a30diasejercicio/Tarjeta.kt
+++ app/src/main/java/com/example/a30diasejercicio/Tarjeta.kt
@@ -1,10 +1,11 @@
 package com.example.a30diasejercicio
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 data class Tarjeta(
-    val nombreEquipo: String,
+    @StringRes val nombreEquipo: Int,
     val anioFormacion: Int,
-    val premios: String,
+    val premios: Int,
     @DrawableRes val imageResourceId: Int
 )
